e54036900ffed1471b37514e38791a8a061c4a70
N1ceeerk <35875832+N1ceeerk@users.noreply.github.com>
2021-03-28 15:30:14 +0300

    break

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const main = document.querySelector(".main");
+const hexNumber = document.querySelector(".main__hex");
+const button = document.querySelector(".main__button");
+
+const hexAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F];
+
+button.addEventListener("click", changeColor);
+
+function changeColor() {}
